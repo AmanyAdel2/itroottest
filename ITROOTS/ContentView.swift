edbc28a9e26c491d59076792f3035248cc3a533d
AmanyAdel2 <amanyadel20242024@gmail.com>
2026-01-27 14:01:55 +0200

    Initial Commit

---
+++ ITROOTS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ITROOTS
+//
+//  Created by Macos on 27/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ITROOTS/ITROOTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITROOTSApp.swift
+//  ITROOTS
+//
+//  Created by Macos on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ITROOTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
